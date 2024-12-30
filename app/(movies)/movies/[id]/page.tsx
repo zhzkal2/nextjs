4d1dddd1e6cafda58f079266be3ef833ffa3556f
zhzkal2 <reliya1541@gmail.com>
2024-12-30 23:56:28 +0900

    Deploy 4.5

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -3,18 +3,23 @@ import React, { Suspense } from 'react';
 import MovieInfo, { getMovie } from "../../../../components/movie-info";
 import MovieVideos from "../../../../components/movie-videos";
 
-interface IParams {
-    params: { id: string };
-}
+type IParams = Promise<{
+    id: string;
+}>;
 
-export async function generateMetadata({ params: { id } }: IParams) {
+
+export async function generateMetadata(props: { params: IParams }) {
+    const params = await props.params;
+    const id = params.id;
     const movie = await getMovie(id);
     return {
         title: movie.title,
     };
 }
 
-export default async function MovieDetailPage({ params: { id } }: IParams) {
+export default async function MovieDetailPage(props: { params: IParams }) {
+    const params = await props.params;
+    const id = params.id;
     return (
         <div>
             <Suspense fallback={<h1>Loading movie info</h1>}>
